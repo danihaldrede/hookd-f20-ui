b5944a28c7431d55b6e5ee623398d0eea3d08503
danihaldrede <59898921+danihaldrede@users.noreply.github.com>
2020-12-07 21:54:32 -0500

    added bootstrap commit

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,35 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <div id="nav">
+            <router-link to="/">Home</router-link>
+            <router-link to="/">About</router-link>
+        </div>
+        <router-view/>
+        <b-navbar fixed="bottom" type="dark" variant="info">
+            <b-navbar-nav class="mx-auto" align="center">
+                <b-nav-item to="Profile">
+                    <b-icon-person-circle/><br>
+                    Profile
+                </b-nav-item>
+                <b-nav-item to="Weather">
+                    <b-icon-sun /><br>
+                    Weather
+                </b-nav-item>
+                <b-nav-item to="/">
+                    <b-icon icon="house-fill"/><br>
+                    Home
+                </b-nav-item>
+                <b-nav-item to="Journal">
+                    <b-icon-journals /><br>
+                    Journal
+                </b-nav-item>
+                <b-nav-item to="FishLog">
+                    <b-icon-map-fill /><br>
+                    FishLog
+                </b-nav-item>
+            </b-navbar-nav>
+        </b-navbar>
     </div>
-    <router-view/>
-  </div>
 </template>
 
 <style lang="scss">
