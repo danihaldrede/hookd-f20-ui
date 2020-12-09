5aed0b69cf0e695ce54754cbccafe388ae0bc07a
danihaldrede <59898921+danihaldrede@users.noreply.github.com>
2020-12-09 01:10:04 -0500

    added fish profile details

--- src/views/Fish.vue
+++ src/views/Fish.vue
@@ -6,5 +6,16 @@
                 <b-button variant="outline-success" class="my-2 my-sm-0" type="submit">Search</b-button>
             </b-nav-form>
         </b-navbar>
+    <b-list-group>
+        <b-list-group-item class="align-side">
+            <b-avatar src="https://www.fws.gov/fisheries/freshwater-fish-of-america/images/originals/east_warm/Largemouth_bass_DuaneRavenArt.png" size="5rem" class="align-center"></b-avatar>
+            Largemouth Bass
+        </b-list-group-item>
+        <b-list-group-item>
+            <b-avatar src="." size="4rem" class="align-center"></b-avatar>
+            Smallmouth Bass
+        </b-list-group-item>
+    </b-list-group>
+
     </div>
 </template>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="mb-5">
-        <h2>Welcome Danielle!</h2>
-            <b-avatar src="https://avatars0.githubusercontent.com/u/59898921?s=60&v=4" size="6rem"></b-avatar>
+        <h2>Welcome!</h2>
+            <b-avatar src="https://avatars0.githubusercontent.com/u/59898921?s=60&v=4" size="8rem"></b-avatar>
+        <h3>Danielle Haldren</h3>
+        <b-button to="/prefer" variant="outline-success">Preferences</b-button>
     </div>
 </template>
